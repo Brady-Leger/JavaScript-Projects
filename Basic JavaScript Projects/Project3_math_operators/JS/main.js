bc227814a2e186ace34f027462458ec0fa0850a7
Brady-Leger <159858263+Brady-Leger@users.noreply.github.com>
2024-03-20 12:24:00 -0600

    Working on Math Object Methods

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,3 +1,46 @@
+window.alert(Math.random());
+
 function Math(a,b) {
     return a + b;
 }
+
+function Math2(a,b) {
+    return a - b;
+}
+
+function Math3(a,b) {
+    return a * b;
+}
+
+function Math4(a,b) {
+    return a / b;
+}
+
+function Math5(a,b,c) {
+    return a / b + c;
+}
+
+function Math6 () {
+    var simple_math = 5 % 2;
+    return simple_math
+}
+
+function Math7 () {
+    var x = 10;
+    document.getElementById("Math7").innerHTML = -x;
+    return x
+}
+
+function Math8 () {
+    var x = 10;
+    x++;
+    document.write(x);
+}
+
+function Math9 () {
+    var y = 10;
+    y--;
+    document.write(y);
+}
+
+var CubeRoot = 100;
